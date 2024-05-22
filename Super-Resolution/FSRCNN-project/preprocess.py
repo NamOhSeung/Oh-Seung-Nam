6271135f3588fdeb97d648f3715cad15c41eb976
NamOhSeung <98510923+NamOhSeung@users.noreply.github.com>
2024-05-22 13:52:10 +0900

    Update preprocess.py

--- Super-Resolution/FSRCNN-project/preprocess.py
+++ Super-Resolution/FSRCNN-project/preprocess.py
@@ -38,7 +38,8 @@ req_size = 20
 inp_size = int(req_size/scale)
 stride = 15
 
-def extract_patches(img, req_size, stride):
+
+def extract_patches(img, req_size, stride): # 패치 추출
     patches = []
     shapes = img.shape
     for i in range(0, shapes[0] - req_size + 1, stride):
@@ -55,11 +56,11 @@ def preprocessing(img_path1, img_path2, req_size, inp_size, stride):
         full_path = os.path.join(img_path1, file_path)
         img = cv2.imread(full_path)
         img = np.asarray(img)
-        patches_HR = extract_patches(img, req_size, stride)
+        patches_HR = extract_patches(img, req_size, stride) # 고해상도 패치 추출
         for patch_HR in patches_HR:
-            patch_LR = cv2.resize(patch_HR, (inp_size, inp_size))
-            inputs.append(patch_LR)
-            labels.append(patch_HR)
+            patch_LR = cv2.resize(patch_HR, (inp_size, inp_size)) # 고해상도 패치 사이즈를 줄여서 저해상도패치 추출
+            inputs.append(patch_LR) # 저해상도 패치 매핑
+            labels.append(patch_HR) # 고해상도 패치 매핑
 
     count = 0
     for file_path in os.listdir(img_path2):
@@ -74,5 +75,22 @@ def preprocessing(img_path1, img_path2, req_size, inp_size, stride):
             patch_LR = cv2.resize(patch_HR, (inp_size, inp_size))
             inputs.append(patch_LR)
             labels.append(patch_HR)
+    
+    # inputs,labels = flip_randomly(inputs,labels)
 
     return inputs, labels
+
+def flip_randomly(lowres_img, highres_img):
+    """이미지를 무작위로 좌우로 뒤집는다."""
+
+    # 0부터 1까지의 균일 분포에서 무작위 숫자를 생성
+    rn = tf.random.uniform(shape=(), maxval=1)
+    # 만약 rn이 0.5보다 작으면 원본 이미지를 반환하고, 그렇지 않으면 뒤집힌 이미지를 반환
+    return tf.cond(
+        rn < 0.5,
+        lambda: (lowres_img, highres_img),
+        lambda: (
+            tf.image.flip_left_right(lowres_img),
+            tf.image.flip_left_right(highres_img),
+        ),
+    )
