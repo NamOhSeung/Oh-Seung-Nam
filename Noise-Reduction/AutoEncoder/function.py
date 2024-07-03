f23b752a0b97f2d884024c2edfbe81b09c4de920
NamOhSeung <98510923+NamOhSeung@users.noreply.github.com>
2024-07-03 23:55:37 +0900

    Update function.py

--- Noise-Reduction/AutoEncoder/function.py
+++ Noise-Reduction/AutoEncoder/function.py
@@ -7,7 +7,7 @@ from tensorflow import keras
 from keras import layers
 
  
-
+ 
 def PSNR(super_resolution, high_resolution):
     """Compute the peak signal-to-noise ratio, measures quality of image."""
     psnr_value = tf.image.psnr(high_resolution, super_resolution, max_val=255)
